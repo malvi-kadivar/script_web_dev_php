eb686eb245fa9220ced8263385f347da56eeb75f
Malvi Kadivar <143687243+malvi-kadivar@users.noreply.github.com>
2023-09-02 13:33:28 +0530

    Add files via upload
    Create a PHP program that uses echo to display the following text with proper formatting:

---
+++ script14.php
@@ -0,0 +1,15 @@
+script14.php
+<?PHP
+$html="<h1>
+		<ol style='list-style:none'>
+			    <li><font color='Orange'>Line</font><font color='red'>1</font></li> 
+			<ol style='list-style:none'>
+				<li><font color='Orange'>Line</font><font color='red'>2</font></li> 
+					<ol style='list-style:none'>
+						<li><font color='Orange'>Line</font><font color='red'>3</font></li> 
+					</ol> 
+			</ol>
+	   </ol> 
+	   </h1>";
+echo $html;
+?>
