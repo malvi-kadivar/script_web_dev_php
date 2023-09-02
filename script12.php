66016ffac69b9bb141d0bf06268b0e0694f05221
Malvi Kadivar <143687243+malvi-kadivar@users.noreply.github.com>
2023-09-02 13:32:49 +0530

    Add files via upload
    Develop a PHP program that uses echo to display an unordered list (<ul>) with three list items (<li>)
    containing different colors.

---
+++ script12.php
@@ -0,0 +1,18 @@
+script12.php
+<?PHP
+$UNORDER ="<UL>
+
+	<LI>
+		<FONT COLOR='RED'> APPLE</FONT>
+	</LI><BR>
+	<LI>
+		<FONT COLOR='BLUE'> MANGO</FONT>
+	</LI><BR>
+	<LI>
+		<FONT COLOR='GREEN'> ORANGE </FONT>
+	</LI><BR>
+</UL>";
+
+	ECHO $UNORDER;
+	
+?>
