c3c9bf6344f59efa5ea1d1d9aba2b159dc1bd5cd
Malvi Kadivar <143687243+malvi-kadivar@users.noreply.github.com>
2023-10-27 21:26:03 +0530

    Add files via upload
    Write a PHP script to use the array_shift() function to remove an element from the
    beginning of an array and the array_unshift() function to add elements at the beginning.

---
+++ task3/script_38.php
@@ -0,0 +1,16 @@
+<?php
+$color=array("pink", "purple", "red", "green", "yellow", "blue");
+print_r($color);
+
+echo "<br>";
+
+array_shift($color);
+print_r($color);
+
+echo "<br>";
+
+array_unshift($color,"Black");
+print_r($color);
+
+
+?>
