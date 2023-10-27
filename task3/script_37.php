c237e082abcfbb8aada16ea4fedb330399f9eb12
Malvi Kadivar <143687243+malvi-kadivar@users.noreply.github.com>
2023-10-27 21:25:17 +0530

    Add files via upload
    Develop a PHP script that uses the array_push() function to add elements to an array and
    the array_pop() function to remove elements from the end.

---
+++ task3/script_37.php
@@ -0,0 +1,15 @@
+<?php
+$color=array("Red", "Green", "Yellow", "Blue");
+print_r($color);
+
+echo "<br>";
+
+array_push($color,"pink");
+print_r($color);
+
+echo "<br>";
+
+array_pop($color);
+print_r($color);
+
+?>
