58d2e11480d52d91a8ed3620100e3711463da796
Malvi Kadivar <143687243+malvi-kadivar@users.noreply.github.com>
2023-10-27 21:29:33 +0530

    Add files via upload
    Write a PHP script that finds the intersection of two arrays using the array_intersect()
    function.

---
+++ task3/script_41.php
@@ -0,0 +1,12 @@
+<?php
+$color = array("green", "yellow", "blue", "pink");
+$fruits = array("banana", "orange", "grapes", "apple", "pink", "green");
+print_r($color);
+
+echo "<br>";
+print_r($fruits);
+echo "<br>";
+
+$itersect=array_intersect($fruits,$color);
+print_r($differences);
+?>
