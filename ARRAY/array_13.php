684710dc1a17530353715a283f505e553fc3e78e
Malvi Kadivar <143687243+malvi-kadivar@users.noreply.github.com>
2023-10-28 08:32:56 +0530

    Add files via upload
    Access and display the person's age from the associative array created in question 12.

---
+++ ARRAY/array_13.php
@@ -0,0 +1,8 @@
+<?php
+//13. Access and display the persons age from the associative array created in question 12.
+$personal_detail = array(
+    "name"=>"Malvi",
+    "age"=>"22",
+    "email"=>"malvikadivar0310@gmail.com");
+echo "The age of the person is ".$personal_detail["age"];
+?>
