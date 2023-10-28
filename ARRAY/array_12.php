d7132cd29c300136948090be875f63247df177bd
Malvi Kadivar <143687243+malvi-kadivar@users.noreply.github.com>
2023-10-28 08:31:44 +0530

    Add files via upload
    Create an associative array representing a person's details, including name, age, and email.

---
+++ ARRAY/array_12.php
@@ -0,0 +1,10 @@
+
+<?php
+//12. Create an associative array representing a person's details, including name, age, and email.
+
+$personal_detail = array(
+    "name"=>"Rahul",
+    "age"=>"23",
+    "email"=>"malvikadivar0310@gmail.com");
+print_r($personal_detail);
+?>
