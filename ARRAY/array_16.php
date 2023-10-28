95b01ba0a7f6dd01650d579b8fcfe060edb7395b
Malvi Kadivar <143687243+malvi-kadivar@users.noreply.github.com>
2023-10-28 08:36:23 +0530

    Add files via upload
    Create an indexed array of cities you want to visit in the future.

---
+++ ARRAY/array_16.php
@@ -0,0 +1,7 @@
+<?php
+//16. Create an indexed array of cities you want to visit in the future. 
+
+$future_cities_visit = array("Tokyo","New York","Moscow","Paris");
+
+print_r($future_cities_visit);
+?>
