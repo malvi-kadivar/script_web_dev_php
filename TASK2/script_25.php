10f469b1de746f9c9dc54003b22efb4e71e115b0
Malvi Kadivar <143687243+malvi-kadivar@users.noreply.github.com>
2023-10-28 13:47:10 +0530

    Add files via upload
    Script to swap 2 nos without using 3rd variable using arithmetic operators.

---
+++ TASK2/script_25.php
@@ -0,0 +1,9 @@
+<?php
+$a=10;
+$b=20;
+$c=$a*$b/$a;
+$d=$a*$b/$b;
+echo "<font color='blue' size=5>The swap value of $a is $c.</font>";
+echo "<br/>";
+echo "<font color='blue' size=5>The swap value of $b is $d.</font";
+?>
